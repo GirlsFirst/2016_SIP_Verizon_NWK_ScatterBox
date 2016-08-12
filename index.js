b7ceafbedd4caef4be68d97be33d5dffb67afecd
glors131 <gjsalas131@gmail.com>
2016-08-12 16:17:30 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-function myFunction(){
-	//change the text on screen to "thanks for visiting"
-	// var words = document.getElementById("welcome");
-	// words.innerHTML= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
-	document.getElementById('welcome').innerHTML="5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
-}
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-function myFunction2(){
-	//change the text on screen to "thanks for visiting"
-	// var words = document.getElementById("welcome");
-	// words.innerHTML="3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe.";
-	document.getElementById('welcome').innerHTML="3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe.";
-}
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-//var option1= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on."
-//var option2= "3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe."
-
-
-
-
-
+        console.log('Received Event: ' + id);
+    }
+};
