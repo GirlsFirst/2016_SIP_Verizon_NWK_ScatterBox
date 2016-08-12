4cccc20ead83731dd96e1e0970d3a8e2e334a650
Trinity Headen <trinityah@gmail.com>
2016-08-12 15:44:18 -0400

    Add files via upload

---
+++ deathly.js
@@ -0,0 +1,6 @@
+function myFunction(){
+	//change the text on screen to "thanks for visiting"
+	// var words = document.getElementById("welcome");
+	// words.innerHTML= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
+	document.getElementById('welcome').innerHTML="5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
+}
