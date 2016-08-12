e2ca45c0f3a51157fe5085f6d3ea25602c7a6963
Trinity Headen <trinityah@gmail.com>
2016-08-12 14:27:45 -0400

    Add files via upload

---
+++ fantasy.js
@@ -0,0 +1,27 @@
+function myFunction5(){
+	//change the text on screen to "thanks for visiting"
+	// var words = document.getElementById("welcome");
+	// words.innerHTML= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
+	document.getElementById('welcome').innerHTML="Congragulations. You get to start your very own text adventure. Now you can journey through the forest in hopes of finding civilization.";
+}
+
+function myFunction(){
+	//change the text on screen to "thanks for visiting"
+	// var words = document.getElementById("welcome");
+	// words.innerHTML= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
+	document.getElementById('welcome').innerHTML="You Died";
+}
+
+		//var btn2 = document.getElementById("btn2");
+		//die.onclick = function(){
+    	//btn2.style.backgroundColor = "blue";
+		//}
+
+
+
+//		if  (document.getElementById('btn2').clicked == true){
+//		setTimeout(function() {
+//	  	window.location.href = "fantasy.html";
+//		}, 1000);
+//	 	window.location.href = "index.html";
+//	 }
---
+++ index.js
@@ -0,0 +1,21 @@
+function myFunction(){
+	//change the text on screen to "thanks for visiting"
+	// var words = document.getElementById("welcome");
+	// words.innerHTML= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
+	document.getElementById('welcome').innerHTML="5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
+}
+
+function myFunction2(){
+	//change the text on screen to "thanks for visiting"
+	// var words = document.getElementById("welcome");
+	// words.innerHTML="3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe.";
+	document.getElementById('welcome').innerHTML="3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe.";
+}
+
+//var option1= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on."
+//var option2= "3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe."
+
+
+
+
+
