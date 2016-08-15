d609421e60616b5e74c8d59a4a404e07cfc11dc2
Trinity Headen <trinityah@gmail.com>
2016-08-14 22:02:16 -0400

    Add files via upload

--- space.js
+++ space.js
@@ -1,4 +1,4 @@
-function myFunction7(){
+/**function myFunction7(){
 		document.getElementById('welcome').innerHTML="Congragulations! Now you have to go outside, and  start their very own text adventure. There will be a blip saying plus 10 points.  It will also show that they choose to journey outside, and on the way out decide to take a map of the terrain";
 
 }
