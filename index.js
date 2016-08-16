a54368ba297923e0daab17a63ddacc2de295b1f9
glors131 <gjsalas131@gmail.com>
2016-08-16 10:37:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ function myFunction2(){
 
 //var option1= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on."
 //var option2= "3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe."
+
+if (position == marker2) {
+	fantasy2.html;
+}
