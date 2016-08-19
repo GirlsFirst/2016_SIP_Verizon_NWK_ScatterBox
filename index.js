a576a100a27df6cd788a4e5996771933eb5af952
Trinity Headen <trinityah@gmail.com>
2016-08-19 11:55:16 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,150 @@
-function myFunction(){
-	//change the text on screen to "thanks for visiting"
-	// var words = document.getElementById("welcome");
-	// words.innerHTML= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
-	document.getElementById('welcome').innerHTML="5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on.";
+if (getCurrentposition == marker) {
+	fantasy.html;
 }
-
-function myFunction2(){
-	//change the text on screen to "thanks for visiting"
-	// var words = document.getElementById("welcome");
-	// words.innerHTML="3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe.";
-	document.getElementById('welcome').innerHTML="3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe.";
-}
-
-//var option1= "5 points added to your level, and 3 hours have passed. You arrive at the farm and see that it is on fire. You also hear screaming, and are unsure if it’s other humans. You have a spark of hope that you are not the only one, but are also cautious to trust people early on."
-//var option2= "3 points added to your level, and 2 hours have passed. You arrive at the small village and notice splatters of green goo everywhere. You are curious to see the source of this goo, but instinct says it isn’t safe."
-
 if (position == marker2) {
 	fantasy2.html;
+}
+if (position == marker3) {
+	fantasy3.html;
+}
+if (position == marker4) {
+	fantasy4.html;
+}
+if (position == marker5) {
+	fantasy5.html;
+}
+if (position == marker6) {
+	fantasy6.html;
+}
+if (position == marker7) {
+	fantasy7.html;
+}
+if (position == marker8) {
+	fantasy8.html;
+}
+if (position == marker9) {
+	fantasy9.html;
+}
+if (position == marker10) {
+	fantasy10.html;
+}
+if (position == marker11) {
+	space.html;
+}
+if (position == marker12) {
+	space2.html;
+}
+if (position == marker13) {
+	space3.html;
+}
+if (position == marker14) {
+	space4.html;
+}
+if (position == marker15) {
+	space5.html;
+}
+if (position == marker16) {
+	space6.html;
+}
+if (position == marker17) {
+	space7.html;
+}
+if (position == marker18) {
+	space8.html;
+}
+if (position == marker19) {
+	space9.html;
+}
+if (position == marker20) {
+	space10.html;
+}
+if (position == marker21) {
+	deathly.html;
+}
+if (position == marker22) {
+	deathly2.html;
+}
+if (position == marker23) {
+	deathly3.html;
+}
+if (position == marker24) {
+	deathly4.html;
+}
+if (position == marker25) {
+	deathly5.html;
+}
+if (position == marker26) {
+	deathly6.html;
+}
+if (position == marker27) {
+	deathly7.html;
+}
+if (position == marker28) {
+	deathly8.html;
+}
+if (position == marker29) {
+	deathly9.html;
+}
+if (position == marker30) {
+	deathly10.html;
+}
+if (position == marker31) {
+	deathly11.html;
+}
+if (position == marker32) {
+	deathly12.html;
+}
+if (position == marker33) {
+	deathly13.html;
+}
+if (position == marker34) {
+	deathly14.html;
+}
+if (position == marker35) {
+	deathly15.html;
+}
+if (position == marker36) {
+	horror.html;
+}
+if (position == marker37) {
+	horror2.html;
+}
+if (position == marker38) {
+	horror3.html;
+}
+if (position == marker39) {
+	horror4.html;
+}
+if (position == marker40) {
+	horror5.html;
+}
+if (position == marker41) {
+	horror6.html;
+}
+if (position == marker42) {
+	horror7.html;
+}
+if (position == marker43) {
+	horror8.html;
+}
+if (position == marker44) {
+	horror9.html;
+}
+if (position == marker45) {
+	horror10.html;
+}
+if (position == marker46) {
+	horror11.html;
+}
+if (position == marker47) {
+	horror12.html;
+}
+if (position == marker48) {
+	horror13.html;
+}
+if (position == marker49) {
+	horror14.html;
+}
+if (position == marker50) {
+	horror15.html;
 }
